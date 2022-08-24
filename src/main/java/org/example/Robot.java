06721d479bd2dd88eb59249c1d6b78d41f434c75
Ryan Etedali <ryanetedali@gmail.com>
2022-08-24 00:30:57 -0500

    instantiated a robot object

---
+++ src/main/java/org/example/Robot.java
@@ -0,0 +1,20 @@
+package org.example;
+
+public class Robot {
+
+
+    //Instance variable
+    public String name;
+    public String weaponName;
+    public int weaponPower;
+
+    //Constructor
+    public Robot() {
+        this.name = "rx36";
+        this.weaponName = "sword";
+        this.weaponPower = 22;
+
+
+    }
+
+}
